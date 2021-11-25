e6c25f53fe55dba108bbe2690487e84e4d5a6589
Justin Szaro <justinszaro@gmail.com>
2021-11-24 23:45:55 -0500

    Made insert into table function standardized

--- dataToSQL.py
+++ dataToSQL.py
@@ -6,48 +6,20 @@ def makeTables(connector, table_names, table_attributes):
         connector.create_table(table_names[i], table_attributes[i])
 
 
-def quotes(string):
-    return '"' + string + '"'
-
-
-def insertIntoAbilityTable(connector, abilityDict, abilityID, ability_1, ability_2, ability_hidden):
-    if abilityDict.get((ability_1 + ability_2 + ability_hidden), None) is None:
-        connector.insert_into_table('Abilities',
-                                    [str(abilityID), quotes(ability_1), quotes(ability_2), quotes(ability_hidden)])
-        abilityDict[ability_1 + ability_2 + ability_hidden] = abilityID
-        abilityID += 1
-    return abilityDict, abilityID
-
-
-def insertIntoSpeciesTable(connector, speciesDict, speciesID, species):
-    if speciesDict.get(species, None) is None:
-        connector.insert_into_table('Species', [str(speciesID), quotes(species)])
-        speciesDict[species] = speciesID
-        speciesID += 1
-    return speciesDict, speciesID
-
-
-def insertIntoStatsTable(connector, statsDict, statsID, total_points, hp, attack, defense, sp_attack, sp_defense,
-                         speed):
-    if statsDict.get((total_points + hp + attack + defense + sp_attack + sp_defense + speed), None) is None:
-        connector.insert_into_table('Stats',
-                                    [str(statsID), total_points, hp, attack, defense, sp_attack, sp_defense, speed])
-        statsDict[total_points + hp + attack + defense + sp_attack + sp_defense + speed] = statsID
-        statsID += 1
-    return statsDict, statsID
-
-
-def insertIntoTypingTable(connector, typingDict, typingID, type_1, type_2):
-    if typingDict.get(type_1 + type_2, None) is None:
-        connector.insert_into_table('Typing', [str(typingID), quotes(type_1), quotes(type_2)])
-        typingDict[type_1 + type_2] = typingID
-        typingID += 1
-    return typingDict, typingID
+def insertIntoTables(connector, table_name, dictionary, ID, values):
+    lst = [ID]
+    for value in values:
+        lst.append(value)
+    if dictionary.get(''.join(values), None) is None:
+        connector.insert_into_table(table_name, lst)
+        dictionary[''.join(values)] = ID
+        ID += 1
+    return dictionary, ID
 
 
 def insertIntoGenerationTable(connector, generationID, generation, regionName):
     if generationID == int(generation):
-        connector.insert_into_table('Generation', [generation, quotes(regionName)])
+        connector.insert_into_table('Generation', [generation, regionName])
         generationID += 1
     return generationID
 
@@ -55,8 +27,8 @@ def insertIntoGenerationTable(connector, generationID, generation, regionName):
 def insertIntoCharacteristicsTable(connector, name, pokedex_number, generation, status, speciesID, typingID, height_m,
                                    weight_kg, abilityID, statsID, catch_rate, percentage_male):
     connector.insert_into_table('Characteristics',
-                                [quotes(name), pokedex_number, generation, quotes(status), str(speciesID),
-                                 str(typingID), height_m, weight_kg, str(abilityID), str(statsID), quotes(catch_rate), quotes(percentage_male)])
+                                [name, pokedex_number, generation, status, str(speciesID),
+                                 str(typingID), height_m, weight_kg, str(abilityID), str(statsID), catch_rate, percentage_male])
 
 
 def insertDataIntoTables(connector, filename):
@@ -70,15 +42,15 @@ def insertDataIntoTables(connector, filename):
             ability_1, ability_2, ability_hidden, total_points, hp, attack, defense, sp_attack, sp_defense, speed, \
             catch_rate, percentage_male, region_name = components
 
-            abilityDict, abilityID = insertIntoAbilityTable(connector, abilityDict, abilityID, ability_1, ability_2,
-                                                            ability_hidden)
-            speciesDict, speciesID = insertIntoSpeciesTable(connector, speciesDict, speciesID, species)
-            statsDict, statsID = insertIntoStatsTable(connector, statsDict, statsID, total_points, hp, attack, defense,
-                                                      sp_attack, sp_defense, speed)
-            typingDict, typingID = insertIntoTypingTable(connector, typingDict, typingID, type_1, type_2)
+            abilityDict, abilityID = insertIntoTables(connector, 'Abilities', abilityDict, abilityID, [ability_1, ability_2,
+                                                            ability_hidden])
+            speciesDict, speciesID = insertIntoTables(connector, 'Species', speciesDict, speciesID, [species])
+            statsDict, statsID = insertIntoTables(connector, 'Stats', statsDict, statsID, [total_points, hp, attack, defense,
+                                                      sp_attack, sp_defense, speed])
+            typingDict, typingID = insertIntoTables(connector, 'Typing', typingDict, typingID, [type_1, type_2])
             generationID = insertIntoGenerationTable(connector, generationID, generation, region_name.strip())
             insertIntoCharacteristicsTable(connector, name, pokedex_number, generation, status, speciesDict[species],
-                                           typingDict[type_1 + type_2], quotes(height_m), quotes(weight_kg),
+                                           typingDict[type_1 + type_2], height_m, weight_kg,
                                            abilityDict[ability_1 + ability_2 + ability_hidden], statsDict[
                                                total_points + hp + attack + defense + sp_attack + sp_defense + speed], catch_rate, percentage_male)
 
